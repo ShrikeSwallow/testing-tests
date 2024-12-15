643beb4808a6a4f7a454c1ee3db48219dbffbd40
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-15 09:27:56 +0000

    Implement and test reverseString function

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,7 @@
 export const capitalize = (string) => {
   return string[0].toUpperCase() + string.substring(1).toLowerCase();
 };
+
+export const reverseString = (string) => {
+  return string.split("").reverse().join("");
+};
--- src/functions.test.js
+++ src/functions.test.js
@@ -1,5 +1,9 @@
-import { capitalize } from "./functions";
+import { capitalize, reverseString } from "./functions";
 
 test("capitalizes string passed as an argument", () => {
   expect(capitalize("test")).toBe("Test");
+});
+
+test("Reverses string passed as an argument", () => {
+  expect(reverseString("monkaS")).toBe("Saknom");
 });
